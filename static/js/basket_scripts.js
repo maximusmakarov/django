a8d53253db78d435267e6d612243537396005649
Maximus <maximus.makarov@mail.ru>
2019-04-11 23:55:36 +0300

    - переделано меню категорий: добавлена подстветка категории "все"   в урлах поставил праильный namespace на продукты - Добавлена страница продуктов - Защита доступа к корзине декоратором @login_required - асинхронное редактирование товаров в корзине через AJAX - испралена ошибка, которая возникает, если пользователь логинится после того, как был переброшен на страницу логина с basket/add - немного видоизменен basket_scripts.js

--- static/js/basket_scripts.js
+++ static/js/basket_scripts.js
@@ -1,5 +1,19 @@
 window.onload = function () {
-    $('.basket-list').on('change', 'input[type="number"]', function (event) {
+    // $('.basket-list').on('change', 'input[type="number"]', function (event) {
+    //     let targetHref = event.target;
+    //
+    //     $.ajax({
+    //         url: "/basket/update/" + targetHref.name + "/" + targetHref.value + "/",
+    //         success: function (data) {
+    //             $('.basket-list').html(data.result);
+    //         }
+    //     });
+    //
+    //     event.preventDefault();
+    // });
+
+    $('.basket-list').change( 'input[type="number"]',function (event) {
+
         let targetHref = event.target;
 
         $.ajax({
@@ -11,4 +25,4 @@ window.onload = function () {
 
         event.preventDefault();
     });
-}
+};
